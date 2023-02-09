2e4e01fcb3ebef7b94efc9d54de067312492933d
Elin Xristozov <73062722+ElinXr@users.noreply.github.com>
2023-02-09 13:03:12 +0200

    Create AdBlock.js

---
+++ AdBlock.js
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<title>
+Изключване на AdBlock 
+</title>
+	<head>
+
+
+		<script>
+			alert('Подкрепете ни като изключите блокиращия софтуер!');
+			window.location = 'file:///C:/Users/Elin/Desktop/R_AdBlock.html' ;
+			window.open('_blank');
+		</script>
+	</head>
+<body onload="myFunction()">
+	
+</body>
+</html>
