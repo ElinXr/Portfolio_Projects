eb1d55bb5b41f58c998031b314d9c3dff9631988
Elin Xristozov <73062722+ElinXr@users.noreply.github.com>
2023-02-09 13:26:38 +0200

    Create TimelineAR.php

---
+++ TimelineAR.php
@@ -0,0 +1,160 @@
+<!--DATA BASE - http://razonartificial.com/themes/reason/v1.6/page_timeline.html 
+PHP is for Netbeans connection -->
+
+
+<?php //$_SESSION['SHOW_ADS'] = false; ?>
+<?php $contacts_link = header_link([CONTROLLER => 'page', ACTION => 'guide']); ?>
+<!DOCTYPE html>
+<html>
+<div class="container">
+    <section>
+        <h3 class="timeline-title"><span>Кандидатстване</span></h3>
+        <div class="row timeline">
+            <div class="col-sm-6 timeline-left">
+                <div class="timeline-event timeline-event-left animated bounceInLeft animation-delay-3">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading"><strong>Стъпка №1</div>
+
+                        <div class="panel-body">
+							<p>Влезте в сайта: <a href="https://praktiki.mon.bg/">praktiki.mon.bg</a>  <p>                      </div>
+                    </div>
+                </div>
+                <div class="timeline-event timeline-event-left animated bounceInLeft animation-delay-9">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №3</div>
+                        <img src="pic2.jpg" width="90" height="60" />
+                        <div class="panel-body">
+						<p>Въведете Full stack Dev (РНР) в "Търсене" и след това изберете бутона "Подробности", за да отворите обявата.</p>                        </div>
+                    </div>
+                </div>
+                <div class="timeline-event timeline-event-left animated bounceInLeft animation-delay-13">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №5</div>
+                        <p>След като сте попълнили своята автобиография, на мястото на бутона "Попълни" ще се появи бутона "Кандидатсвай" , който трябва да изберете.</p>
+                    </div>
+                </div>
+				<div class="timeline-event timeline-event-left animated bounceInLeft animation-delay-13">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №7</div>
+                        <p>След потвърждението трябва да изберете Вашия академичен наставник (ако нямате предпочитания за такъв, може да изберете Емил Хаджиколев или Станка Хаджиколева).</p>
+                    </div>
+                </div>
+				<div class="timeline-event timeline-event-left animated bounceInLeft animation-delay-13">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №9</div>
+					<p>След като вече имате и ментор, трябва да разпределите необходимите часове (240 часа) в график за период от 6 месеца. Това може да направите като отидете в секция "Избрана практика" и натиснете бутона "Редакция на график". При натискането му ще се появи календар, в който първо трябва да запишете часовете за деня на започването на стажа, а след това останалите. Разпределянето на часовете се случва при натискането на всяка дата от календара, след което излиза прозорец, в който трябва да въведете начален час и броят часове, които ще присъствате. Полезен съвет е да разпределите часовете си по такъв начин, за да може стажът да продължи по-кратко време. Също така е добре да не разпределяте часове само за 4-5 или по-малко дни в един месец.</p>                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 timeline-right">
+                <div class="timeline-event timeline-event-right animated bounceInRight animation-delay-5">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №2</div>
+						<img src="page\css\pic1.jpg" alt="picture" id="pic1">
+                        <div class="panel-body">
+							<p>Изберете от менюто "Обяви" &#10095; "Всички обяви".</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="timeline-event timeline-event-right animated bounceInRight animation-delay-7">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №4</div>
+                        <div class="panel-body">
+							<p>Преди да кандидатствате трябва да попълните своята автобиография.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="timeline-event timeline-event-right animated bounceInRight animation-delay-11">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №6</div>
+                        <img src="assets/img/demo/w11.jpg" class="img-responsive" alt="Image">
+                        <div class="panel-body">
+							<p>В случай, че сте одобрени, следва Вие да потвърдите, че искате да се обучавате в тази фирма. Това може да направите като отидете в секция
+                "Обяви" &#10095; "Моите обяви" и натиснете бутона "Потвърждавам" на съответната обява.</p>
+                        </div>
+                    </div>
+                </div>
+				<div class="timeline-event timeline-event-right animated bounceInRight animation-delay-11">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №8</div>
+                        <img src="assets/img/demo/w11.jpg" class="img-responsive" alt="Image">
+                        <div class="panel-body">
+							<p>Трябва да изчакате потвърждение от страна на академичния наставник и фирмата да Ви назначи ментор.</p>
+                        </div>
+                    </div>
+                </div>
+<div class="timeline-event timeline-event-right animated bounceInRight animation-delay-11">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №10</div>
+                        <img src="assets/img/demo/w11.jpg" class="img-responsive" alt="Image">
+                        <div class="panel-body">
+							<p>Когато графикът Ви е готов трябва да изчакате потвърждение от ментора Ви и академичния Ви наставник.  </p>                      </div>
+                    </div>
+                </div>
+            </div>
+
+
+		</div>
+    </section>
+    <section>
+        <h3 class="timeline-title"><span>Необходими документи</span></h3>
+        <div class="row timeline">
+            <div class="col-sm-6 timeline-left">
+                <div class="timeline-event timeline-event-left animated bounceInLeft animation-delay-13">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №1</div>
+                        <img src="assets/img/demo/w3.jpg" class="img-responsive" alt="Image">
+                        <div class="panel-body">
+							<p>След като е одобрен графика от двете страни, в секцията "Избрана практика" най-долу се появява бутон „Договор с ВУ“, който трябва да изберете, за да попълните данните си в договора.    </p>
+						</div>
+                    </div>
+                </div>
+                <div class="timeline-event timeline-event-left animated bounceInLeft animation-delay-15">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №3</div>
+                        <img src="assets/img/demo/w8.jpg" class="img-responsive" alt="Image">
+                        <div class="panel-body">
+<p>За да генерирате Вашите документи, трябва да се върнете в началото на същата страница и да натиснете бутона "Генериране", след което да изтеглите документите на Вашето устройство.</p>                        </div>
+                    </div>
+                </div>
+				<div class="timeline-event timeline-event-left animated bounceInLeft animation-delay-15">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №5</div>
+                        <img src="assets/img/demo/w8.jpg" class="img-responsive" alt="Image">
+                        <div class="panel-body">
+					<p>Документите трябва да бъдат оставени в Нова сграда на Пловдивския университет, в каб. 330 за подпис и печат.</p>
+
+                    </div>
+                </div>
+            </div>
+			</div>
+            <div class="col-sm-6 timeline-right">
+                <div class="timeline-event timeline-event-right animated bounceInRight animation-delay-13">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №2</div>
+                        <div class="panel-body">
+							<p>След като отворите „Договор с ВУ“ , трябва да попълните всички полета, като накрая е нужно да напишете паролата Ви за вход в<a href="https://praktiki.mon.bg/" target="_blaked"> https://praktiki.mon.bg/ </a>. Ако не знаете Вашия IBAN, се обърнете към служител в банката, на която сте клиент. След попълване на данните, натиснете бутона "Запиши".</p>                        </div>
+                    </div>
+                </div>
+                <div class="timeline-event timeline-event-right animated bounceInRight animation-delay-17">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №4</div>
+                        <img src="assets/img/demo/w6.jpg" class="img-responsive" alt="Image">
+                        <div class="panel-body">
+							<p> Разпечатайте документите в 3 екземпляра и ги попълнете и подпишете.</p>
+                        </div>
+                    </div>
+                </div>
+				<div class="timeline-event timeline-event-right animated bounceInRight animation-delay-17">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Стъпка №6</div>
+                        <img src="assets/img/demo/w6.jpg" class="img-responsive" alt="Image">
+                        <div class="panel-body">
+							<p>Когато предавате документите, ще е необходимо да попълните на място декларация за лична банкова сметка.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+</html>
